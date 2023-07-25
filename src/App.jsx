cd0bf8edc1eb497a56b3df79cc68de581c97adcd
Kevin Li <kevinli617@gmail.com>
2023-07-25 01:41:23 -0400

    update wallpaper reference

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import { Button, Container } from "react-bootstrap";
 import Desktop from "./components/Desktop";
+import wallpaper from "./assets/wallpaper.png";
 
 function App() {
   return (
     <Container
       style={{
-        backgroundImage: "url('/src/assets/wallpaper.png')",
+        backgroundImage: `url(${wallpaper})`,
         backgroundSize: "cover",
         width: "1600px",
         height: "1000px",
