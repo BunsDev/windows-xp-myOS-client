0496ffa9efdff41abaf4909843797c69b52fd753
Kevin Li <kevinli617@gmail.com>
2023-07-29 10:55:55 -0400

    added splash screen

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -3,21 +3,53 @@ import Draggable from "react-draggable";
 
 const Splash = (props) => {
   const { showSplash, setShowSplash } = props;
+
+  const handleClose = () => {
+    setShowSplash(false);
+  };
+
   return showSplash ? (
     <Draggable
       defaultPosition={{ x: 0, y: 0 }}
-      positionOffset={{ x: "280%", y: "3%" }}
+      positionOffset={{ x: "290%", y: "3%" }}
     >
       <Container
-        style={{ width: "400px", height: "300px", backgroundColor: "#ebe9d8" }}
+        style={{ width: "400px", height: "340px", backgroundColor: "#ebe9d8" }}
         className="position-absolute p-0 m-0"
       >
         <Container className="d-flex bg-dark text-light justify-content-between p-0 m-0">
-          <span className="px-3">Welcome to myOS</span>
+          <span className="px-3">myOS Welcome Page</span>
 
-          <button>X</button>
+          <button onClick={handleClose}>X</button>
         </Container>
-        <Container>Welcome to my application.</Container>
+        <Container className="px-2 mx-1 mt-1">
+          <h6>Welcome to myOS Virtual Desktop Environment</h6>
+          <ul>
+            <li>Created entirely with React / PostgreSQL</li>
+            <li>Fully functioning desktop applications</li>
+            <li>
+              AOL Instant Messenger
+              <ul>
+                <li>User authentication w/ password hashing</li>
+                <li>AI-powered "SmarterChild" chatbot</li>
+              </ul>
+            </li>
+            <li>
+              Internet Explorer
+              <ul>
+                <li>Explore my other projects via bookmarks</li>
+                <li>Access other webpages using address bar</li>
+              </ul>
+            </li>
+            <li>Notepad - Retrieval of files from SQL API</li>
+          </ul>
+          <h4 className="text-center">
+            Developed By:{" "}
+            <a href="https://github.com/kevinleet" target="_blank">
+              Kevin Li
+            </a>
+          </h4>
+        </Container>
       </Container>
     </Draggable>
   ) : null;
